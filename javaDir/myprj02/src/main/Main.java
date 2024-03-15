8f4d9449b619e935dcdc6bfd8f6d1a3f57915da7
DESKTOP-CQ2TBPU\user1 <tndls5304@naver.com>
2024-03-15 15:05:40 +0900

    두번쨰 커밋~~ 컴~~~~~

--- javaDir/myprj02/src/main/Main.java
+++ javaDir/myprj02/src/main/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("두번쨰 메인이당~");
+		System.out.println("111111");
+		System.out.println("221111");
+		System.out.println("223331111");
+		System.out.println("155511");
 
 	}
 
